728b48d2e053b598a70e2a8fd70232189098ed4e
risibaba <rishibankar16@gmail.com>
2026-02-06 23:55:42 +0530

    practice

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,30 @@
-const http = require('http');
+// const http = require('http');
 const express = require('express');
+const mongoose = require('mongoose');
 const users = require('./MOCK_DATA.json')
+
+
 
 
 const app = express(); 
 
+mongoose.connect('mongodb://127.0.0.1:27017/blablu')
+.then(()=>{
+    console.log('Connected to MongoDB');
+})
+.catch((error)=>{
+    console.error('Error connecting to MongoDB:', error);
+});
+const schema = new mongoose.Schema({
+    id: Number,
+    first_name: String,
+    last_name: String,
+    email: String,
+    gender  : String,
+})
+
+
+const User = mongoose.model('User', schema);
 
 app.get('/api/users',(req,res)=>{
     res.json(users);
