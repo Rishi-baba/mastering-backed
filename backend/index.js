6b4e6aa39273794415924f7e72ab68546a0ebd66
risibaba <rishibankar16@gmail.com>
2026-02-06 15:26:05 +0530

    completing express

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,29 @@
 const http = require('http');
 const express = require('express');
+const users = require('./MOCK_DATA.json')
+
 
 const app = express(); 
 
-app.get('/about', (req, res) => {
-    res.send('Hello World from Express!'+ 'hey' + req.query.name);  
+
+app.get('/api/users',(req,res)=>{
+    res.json(users);
 })
+
+app.get('/users',(req,res)=>{
+    res.json(users);
+})
+
+
+app.get('/api/users/:id', (req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id)
+})
+
+
+
+ 
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
